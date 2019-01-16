e886602de923e9d306bbd5239f3cc86421afb369
Donat Marko <marko.donat@gmail.com>
2019-01-16 20:20:26 +0100

    :tada: Doesn't do anything yet

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Automatic ATIS generator PHP script for vACCHUN
+ * Created by Donat Marko (VATSIM CID 1389983)
+ * @author     Donat Marko <sdf@donatus.hu>
+ * @copyright  2019 Donat Marko | www.donatus.hu
+ * @license    https://www.gnu.org/licenses/gpl.txt  GNU General Public License v3
+ */
+
+require_once 'vendor/autoload.php';
+use MetarDecoder\MetarDecoder;
